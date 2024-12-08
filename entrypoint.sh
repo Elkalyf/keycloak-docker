b155f64b301357e77d18a14168b44e539c86705e
ChakibGo <dirtysouthside7@gmail.com>
2024-12-08 10:28:27 +0100

    hostname-url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Use Render's PORT variable, defaulting to 8080 if not set
@@ -6,7 +7,5 @@ PORT=${PORT:-8080}
 # Start Keycloak with the correct port and hostname configuration
 exec /opt/keycloak/bin/kc.sh start-dev \
   --http-port=$PORT \
-  --hostname=keycloak-docker-rxfn.onrender.com \
-  --hostname-strict=false \
-  --hostname-admin-url=https://keycloak-docker-rxfn.onrender.com \
   --hostname-url=https://keycloak-docker-rxfn.onrender.com
+  --hostname-strict=false
