75377d32821748003869dfae2e415c6306421a99
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-08 15:54:03 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@
 # Use Render's PORT variable, defaulting to 10000
 PORT=${PORT:-10000}
 
-# Set H2 database URL to prevent unexpected shutdown
-export KC_DB_URL=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE
 
 # Start Keycloak
 exec /opt/keycloak/bin/kc.sh start-dev \
