7e640f4685e4c858925aaeafcf3bfee3e52ae0cd
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-08 13:50:37 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,11 @@
 PORT=${PORT:-8080}
 
 # Start Keycloak with HTTPS configuration
-exec /opt/keycloak/bin/kc.sh start --optimized \
+exec /opt/keycloak/bin/kc.sh start-dev 
   --http-port=$PORT \
   --hostname=https://keycloak-docker-rxfn.onrender.com \
   --hostname-strict=false \
+
 
 
 
