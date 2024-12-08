9d991ea268789a2ad019b3931d5bfe02874971a5
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-08 13:04:08 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ PORT=${PORT:-8080}
 # Start Keycloak with HTTPS configuration
 exec /opt/keycloak/bin/kc.sh start --optimized \
   --http-port=$PORT \
-  --https-port=8443 \
-  --https-key-store-file=/opt/keycloak/conf/tls.key \
-  --https-key-store-password=password \
   --hostname=https://keycloak-docker-rxfn.onrender.com \
-  --hostname-strict=false
+  --hostname-strict=false \
+  --https-protocols=
+
+
