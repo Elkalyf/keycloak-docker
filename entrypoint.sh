ec9fe6da94d1845521dfe0593179b837b19cd692
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-08 18:06:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,13 @@
 # Use Render's PORT variable, defaulting to 10000
 PORT=${PORT:-10000}
 
-
-# Start Keycloak
+# Start Keycloak with PostgreSQL
 exec /opt/keycloak/bin/kc.sh start-dev \
   --http-port=$PORT \
   --hostname=keycloak-docker-rxfn.onrender.com \
   --hostname-strict=false \
-  -Djboss.bind.address=0.0.0.0
+  -Djboss.bind.address=0.0.0.0 \
+  --db=postgres \
+  --db-url=${DATABASE_URL} \
+  --db-username=${PGHERO_USERNAME} \
+  --db-password=${PGHERO_PASSWORD}
