5fccd9c0bceb2a1762e8fc4f4f8a44d6af351c35
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-10 14:21:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,12 @@ PORT=${PORT:-10000}
 # Build the Keycloak server
 /opt/keycloak/bin/kc.sh build
 
+# Start the Keycloak server
 exec /opt/keycloak/bin/kc.sh start \
     --http-port=$PORT \
     --hostname=${RENDER_EXTERNAL_HOSTNAME:-localhost} \
     --hostname-strict=false \
     --http-enabled=true \
-    --verbose
+    --proxy=edge \
+    --verbose \
+    --bind-address=0.0.0.0
