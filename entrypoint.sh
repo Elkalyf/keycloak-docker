13e1e165aaba34f94bfa051adcbb6f1fe6229aec
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-10 13:10:54 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ exec /opt/keycloak/bin/kc.sh start \
      --hostname=keycloak-docker-rxfm.onrender.com \
      --hostname-strict=false \
      --http-enabled=true \
-     --proxy-headers=xforwarded \
+     --proxy=edge \
      --verbose
 
 
