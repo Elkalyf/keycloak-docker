1cc5fddfd4c94fff67df449f4c64ef933848a3e2
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-10 12:02:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 PORT=${PORT:-8080}
 
 # Start Keycloak with H2 database
-exec /opt/keycloak/bin/kc.sh start-dev \
+exec /opt/keycloak/bin/kc.sh start-dev
   --http-port=${PORT} \
   --hostname=keycloak-docker-rxfm.onrender.com \
   --hostname-strict=false \
