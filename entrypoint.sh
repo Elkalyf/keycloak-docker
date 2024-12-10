88d4d60aefe087e1eb994e17e2f7ec65d0e86af1
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-10 13:26:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,13 @@
 PORT=${PORT:-10000}
 
 # Build the Keycloak server
-/opt/keycloak/bin/kc.sh build
+/opt/keycloak/bin/kc.sh build --optimized
 
 # Start the Keycloak server
 exec /opt/keycloak/bin/kc.sh start \
      --http-port=$PORT \
      --hostname=keycloak-docker-rxfm.onrender.com \
      --hostname-strict=false \
-     --http-enabled=true \
      --verbose
-
 
   
