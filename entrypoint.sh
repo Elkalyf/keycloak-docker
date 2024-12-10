b96807a5134c8c040b096cddbbb025ad95a2eb2d
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-10 15:36:29 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,17 @@
 # Use Render's PORT variable (default to 10000 if not set)
 PORT=${PORT:-10000}
 
-# Use Render's external hostname if available, otherwise fallback to '0.0.0.0'
-HOSTNAME=${RENDER_EXTERNAL_HOSTNAME:-0.0.0.0}
-
 # Build the Keycloak server
 /opt/keycloak/bin/kc.sh build
 
-# Start the Keycloak server with proper binding
+# Start the Keycloak server
 exec /opt/keycloak/bin/kc.sh start \
     --http-port=$PORT \
-    --hostname=$HOSTNAME \
+    --hostname=keycloak-docker-rxfm.onrender.com \
     --hostname-strict=false \
     --http-enabled=true \
-    --cache=local \
     --spi-hostname-default=0.0.0.0 \
-    --spi-hostname-strict-backchannel=false \
+    --spi-cors-allowed-origin-pattern=https://keycloak-docker-rxfm.onrender.com \
+    --log-level=DEBUG \
+    --cache=local \
     --verbose
