ea935143ec515fe62eab1aaf1e5e94376537bd0b
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-10 22:18:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Use the port provided by Render (default to 10000 if not set)
+# Use the port provided by Render or default to 10000
 PORT=${PORT:-10000}
 
-# Build the Keycloak server
-/opt/keycloak/bin/kc.sh build
+# Set Keycloak environment variables
+export JAVA_OPTS="-Dquarkus.http.host=0.0.0.0 -Dquarkus.http.port=${PORT}"
 
-# Start Keycloak with the correct binding
+# Start Keycloak with the correct options
 exec /opt/keycloak/bin/kc.sh start \
-    --http-port=$PORT \
-    --hostname=0.0.0.0 \
+    --http-port=${PORT} \
     --hostname-strict=false \
     --http-enabled=true \
     --cache=local \
-    --log-level=DEBUG \
-    --verbose
+    --log-level=DEBUG
