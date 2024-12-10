9131555cb833c272a8ce3ce90c07e071348af67a
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-10 21:28:39 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Use Render's PORT variable (default to 10000 if not set)
+# Use the port provided by Render (default to 10000 if not set)
 PORT=${PORT:-10000}
 
 # Build the Keycloak server
 /opt/keycloak/bin/kc.sh build
 
-# Start the Keycloak server with proper binding
+# Start Keycloak with the correct binding
 exec /opt/keycloak/bin/kc.sh start \
     --http-port=$PORT \
-    --hostname=${RENDER_EXTERNAL_HOSTNAME:-localhost} \
+    --hostname="${RENDER_EXTERNAL_HOSTNAME:-localhost}" \
     --hostname-strict=false \
     --http-enabled=true \
-    --spi-cors-allowed-origin-pattern="https://${RENDER_EXTERNAL_HOSTNAME}" \
-    --spi-login-protocol-openid-connect-csp-frame-src-value="'self' https://${RENDER_EXTERNAL_HOSTNAME}" \
     --cache=local \
     --log-level=DEBUG \
     --verbose
