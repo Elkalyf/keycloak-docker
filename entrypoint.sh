28519107a1bec8f746c3f5a7ab4901aac62af147
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-10 20:33:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Use Render's PORT variable (default to 10000 if not set)
-PORT=${PORT:-8843}
+# Use Render's PORT variable (default to 8443 if not set)
+PORT=${PORT:-8443}
 
 # Build the Keycloak server
 /opt/keycloak/bin/kc.sh build
 
 # Start the Keycloak server with proper binding
 exec /opt/keycloak/bin/kc.sh start \
-    --http-port=$PORT \
+    --https-port=$PORT \  # Use `--https-port` instead of `--http-port` if you're focusing on HTTPS
     --hostname=keycloak-docker-rxfn.onrender.com \
     --hostname-strict=false \
-    --http-enabled=true \
+    --http-enabled=false \  # Disables HTTP to avoid conflicts with Render's proxy
     --cache=local \
     --spi-cors-allowed-origin-pattern=https://keycloak-docker-rxfn.onrender.com \
     --spi-login-protocol-openid-connect-csp-frame-src-value="'self' https://keycloak-docker-rxfn.onrender.com" \
     --log-level=DEBUG \
     --verbose
+
