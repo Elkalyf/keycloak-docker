8d61d30054e435e722ed9a01711072b65d568afe
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-10 20:08:47 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,14 @@ PORT=${PORT:-10000}
 # Build the Keycloak server
 /opt/keycloak/bin/kc.sh build
 
-# Start the Keycloak server
+# Start the Keycloak server with proper binding
 exec /opt/keycloak/bin/kc.sh start \
     --http-port=$PORT \
     --hostname=keycloak-docker-rxfn.onrender.com \
     --hostname-strict=false \
     --http-enabled=true \
-    --spi-hostname-default=0.0.0.0 \
-    --spi-cors-allowed-origin-pattern=https://keycloak-docker-rxfn.onrender.com \
-    --log-level=DEBUG \
     --cache=local \
+    --spi-cors-allowed-origin-pattern=https://keycloak-docker-rxfn.onrender.com \
+    --spi-login-protocol-openid-connect-csp-frame-src-value="'self' https://keycloak-docker-rxfn.onrender.com" \
+    --log-level=DEBUG \
     --verbose
