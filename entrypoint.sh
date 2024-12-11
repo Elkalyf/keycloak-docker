17f08c74be0f6db2e6d568812a6865b47b937289
Elkalyf <40056537+Elkalyf@users.noreply.github.com>
2024-12-11 14:58:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ export JAVA_OPTS="-Djava.net.preferIPv4Stack=true -Dkeycloak.profile.feature.upl
 
 # Start the Keycloak server with HTTPS enabled
 exec /opt/keycloak/bin/kc.sh start \
-    --http-port=$PORT \
-    --https-port=8443 \
+    --http-port=8080 \
+    --https-port=$PORT \
     --https-certificate-file=/etc/x509/https/cert.pem \
     --https-certificate-key-file=/etc/x509/https/key.pem \
     --hostname=keycloak-docker-rxfm.onrender.com \
